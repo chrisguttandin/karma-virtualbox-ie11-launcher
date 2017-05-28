de15d1a84c47ef1ac1f26abbbb1bb723c88337a7
chrisguttandin <chrisguttandin@media-codings.com>
2017-05-28 22:59:29 +0200

    fix test

--- test/unit/module.js
+++ test/unit/module.js
@@ -3,7 +3,7 @@ const virtualBoxIE11Launcher = require('../../src/module.js');
 describe('karma-virtualbox-ie11-launcher', () => {
 
     it('should export the launcher', () => {
-        expect(virtualBoxIE11Launcher['launcher:VirtualBoxIE11']).to.defined;
+        expect(virtualBoxIE11Launcher['launcher:VirtualBoxIE11']).to.not.be.undefined;
     });
 
 });
