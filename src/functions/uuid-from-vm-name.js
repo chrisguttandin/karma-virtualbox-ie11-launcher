f2dbb1cdccdae99698a585edf149096adc29fc52
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-11-29 17:56:22 +0100

    style: use shorthand for regular expression

--- src/functions/uuid-from-vm-name.js
+++ src/functions/uuid-from-vm-name.js
@@ -1,4 +1,4 @@
-const UUID_REGEX = /^UUID="(?<uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})"$/m;
+const UUID_REGEX = /^UUID="(?<uuid>[\da-f]{8}(?:-[\da-f]{4}){3}-[\da-f]{12})"$/m;
 
 module.exports.uuidFromVMName = (execute, vmName, uuid, log) => {
     if (uuid) {
